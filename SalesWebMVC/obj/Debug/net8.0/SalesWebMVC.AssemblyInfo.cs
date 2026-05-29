ab91e77910efc46d6cb4a075ed29ca897f0a0983
Romario Ribeiro <romarioribeirodev@gmail.com>
2026-05-28 21:17:39 -0300

    Crianddo CRUD com Scaffolding

--- SalesWebMVC/obj/Debug/net8.0/SalesWebMVC.AssemblyInfo.cs
+++ SalesWebMVC/obj/Debug/net8.0/SalesWebMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+615c9004be2e0cfa86498fdd33f1c89b8e2d39df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b43960b07ecedb670eabf2bf33d151e1596935de")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
